d1554e06d0b9013cc12dfeb3c402ea092ec8fe14
pythorine <pythorine@gmail.com>
2025-06-19 21:28:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,38 @@ import random
 rounds_played = 0
 enemy_win = 0
 user_win = 0
-weapons = ["rock","paper","scissors"]
+choices = ["rock","paper","scissors"]
+rounds = 0
 
 
-#starts initalizing choices from both sides
-user_weapon = int(input("Choose your weapon\n\n1. Rock\n2. Paper\n3. Scissors\n\n"))
+#ask user how many rounds they want to play
+rounds = int(input("How many rounds to you want to play? "))
 
-if not isinstance(user_weapon, int):
-    user_weapon = input("Please select your weapon with a number only\n\n1. Rock\n2. Paper\n3. Scissors\n\n")
-else:
-    print("You have selected: " + weapons[user_weapon - 1].capitalize())
-    weapon = random.choice(weapons)
-    print("The enemy has selected: " + weapon.capitalize())
 
-#Problem area - it should allow the user & enemy to re-select their choices - currently it runs the for loop x amount in range() - call a function maybe?
-for i in range(5): 
-    #function here?    
+#function to get and verify choices
+def get_choices():
+    global user_choice
+    global opponent_choice
+    user_choice = int(input("Choose your choice\n\n1. Rock\n2. Paper\n3. Scissors\n\n"))
+
+    if not isinstance(user_choice, int):
+        user_choice = input("Please select your choice with a number only\n\n1. Rock\n2. Paper\n3. Scissors\n\n")
+    else:
+        print("You have selected: " + choices[user_choice - 1].capitalize())
+        opponent_choice = random.choice(choices)
+        print("The enemy has selected: " + opponent_choice.capitalize())
+
+
+#gameloop
+for i in range(rounds-1): 
+    get_choices()
     
-    if weapons[user_weapon-1] == weapon :
+    if choices[user_choice-1] == opponent_choice :
         print("Draw!")
         rounds_played = rounds_played + 1
-    elif (weapons[user_weapon - 1] == "rock" and weapon == "scissors") or \
-         (weapons[user_weapon - 1] == "paper" and weapon == "rock") or \
-         (weapons[user_weapon - 1] == "scissors" and weapon == "paper"):
+    elif (choices[user_choice - 1] == "rock" and opponent_choice == "scissors") or \
+         (choices[user_choice - 1] == "paper" and opponent_choice == "rock") or \
+         (choices[user_choice - 1] == "scissors" and opponent_choice == "paper"):
         
         print("You win!")
         rounds_played = rounds_played + 1
@@ -38,3 +47,8 @@ for i in range(5):
         enemy_win = enemy_win + 1
         print("You have played " + str(rounds_played) + " rounds!")
         print("Current result: " + str(user_win) + ":" + str(enemy_win))
+
+### add maximum amount of rounds to be played
+### verify that user can only enter integers
+### add winning screen
+### improve gameloop print text
